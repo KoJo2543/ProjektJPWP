163672ecfb7168a37f5021969380a3564a51776e
Konrad <117116168+KoJo2543@users.noreply.github.com>
2023-01-18 21:52:44 +0100

    finalna wersja12.01.23
    nie pamietam czy wrzucalem

--- Main.java
+++ Main.java
@@ -1,7 +1,32 @@
-//prealfa
-
+/*
+        * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+        * Copyright 2023 Gdansk University of Technology,
+        * Dept. of Electronics and telecommunication engineering, Gdansk, Poland
+        * All rights reserved. Use is subject to license terms.
+        *
+        * This file is available and licensed under the following license:
+        *
+        * Redistribution and use in source and binary forms, with or without
+        * modification, are not permitted without written permission from the copyright
+        * holders.
+        *
+        * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+        * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+        * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+        * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+        * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+        * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+        * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+        * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+        * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        */
+/** klasa glowna */
 public class Main {
 
+    /** tworzenie okna gry
+     * @param args
+     */
     public static void main(String[] args){
 
         new MyFrame();
--- MyFrame.java
+++ MyFrame.java
@@ -1,5 +1,26 @@
-//kkkk
-
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * Copyright 2023 Gdansk University of Technology,
+ * Dept. of Electronics and telecommunication engineering, Gdansk, Poland
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are not permitted without written permission from the copyright
+ * holders.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,32 +29,47 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
 
-
+/** klasa zawierajaca tworzenie okna gry oraz rozgrywki
+ * @author Konrad Cieślak
+ */
 public class MyFrame extends JFrame implements ActionListener{
 
 
-    //rozmiar okna
+    /** szerokosc okna */
     final int szerokosc = 1280;
+    /** wysokosc okna */
     final int wysokosc = 720;
-    //-----------------------
-    //generowanie wsp pojawienia sie celu
+    /** zmienna pomocnicza do losowania x celu */
     Random xlos = new Random();
+    /** zmienna pomocnicza do losowania y celu */
     Random ylos = new Random();
+    /** losowanie wspolrzednej x celu */
     int x = xlos.nextInt(1152);
+    /** losowanie wspolrzednej y celu */
     int y= ylos.nextInt(492)+100;
-    //---------------------------------
-    float pkt = 0; //ilosc trafien uzyskanych
-    float klik = 0; //ilosc prob trafienia
+    /** liczba punktow punktow */
+    float pkt = 0;
+    /** liczba klikniec */
+    float klik = 0;
+    /**  skutecnzosc trafien */
     float skutecznosc;
-    boolean started = false; //czy gra trwa
-    int lcelow =20; //ile celow pojawi sie podczas rozgrywki (zalezy od trudnosci)
-    //rozmiar celu (zalezy od poziomu trudnosci domyslnie normalny)
+    /** zmienna informujaca o trwaniu gry */
+    boolean started = false;
+    /** liczba celow */
+    int lcelow =20;
+    /** szerokosc celu */
     int celszer=64;
+    /** wysokosc celu */
     int celwys=64;
+    /** timer */
     Timer timer;
+    /** czas trwania gry */
     float czas;
-    String[] poziom={"Normalny","Łatwy","Trudny"};
-    JLabel label;
+    /** tabela z dostepnymi poziomami trudnosci */
+    String[] poziom={"Normalny","Latwy","Trudny"};
+    /** cel */
+    JLabel cel;
+    /** combobox z wyborem poziomu trudnosci */
     JComboBox poziomt;
 
 
@@ -53,7 +89,7 @@ public class MyFrame extends JFrame implements ActionListener{
 
             //----------------------------
 
-            //menu
+            /** panel tworzacy menu gry */
             JPanel menu = new JPanel();
             menu.setBounds(0,0,1280,100);
             menu.setLayout(null);
@@ -62,7 +98,7 @@ public class MyFrame extends JFrame implements ActionListener{
             this.add(menu);
             //------------------------------------
 
-            //części menu
+            /** guzik rozpoczynajacy gre */
             JButton start = new JButton();
             start.setText("START");
             start.setBounds(300,8,120,50);
@@ -70,12 +106,14 @@ public class MyFrame extends JFrame implements ActionListener{
             start.setOpaque(true);
             menu.add(start);
 
+            /** guzik przerywajacy gre */
             JButton stop = new JButton();
             stop.setText("PRZERWIJ");
             stop.setBounds(425,8,120,50);
             stop.setBackground(Color.LIGHT_GRAY);
             menu.add(stop);
 
+            /** pole pokazujace statystyki gry */
             JLabel punkty = new JLabel();
             punkty.setBounds(0,66,1280,32);
             punkty.setBackground(Color.LIGHT_GRAY);
@@ -98,12 +136,14 @@ public class MyFrame extends JFrame implements ActionListener{
             menu.add(punkty);
             punkty.setVisible(true);
 
+            /** guzik do zamykania gry */
             JButton exit = new JButton();
             exit.setText("WYJDŹ");
             exit.setBounds(1100,8,120,50);
             exit.setBackground(Color.LIGHT_GRAY);
             menu.add(exit);
 
+            /** pole pokazujace logo gry */
             JLabel logomenu = new JLabel(new ImageIcon("logomenu.png"));
             logomenu.setSize(120,50);
             logomenu.setLocation(8,8);
@@ -118,11 +158,11 @@ public class MyFrame extends JFrame implements ActionListener{
             //-------------------------------------------
             timer = new Timer(60,this);
             //cel
-            label = new JLabel(new ImageIcon("cel.png"));
-            this.add(label);
+            cel = new JLabel(new ImageIcon("cel.png"));
+            this.add(cel);
             //label.setBackground(Color.black);
-            label.setOpaque(true);
-            label.setSize(celszer,celwys);
+            cel.setOpaque(true);
+            cel.setSize(celszer,celwys);
             //----------------------------------------
 
 
@@ -136,7 +176,11 @@ public class MyFrame extends JFrame implements ActionListener{
 
                 }
 
+                /** zwiekszanie ilosci klikniec i liczenie skutecznosci
+                 * @param e
+                 */
                 @Override
+
                 public void mousePressed(MouseEvent e) {
                    if(started == true) {
                        klik++;
@@ -169,19 +213,23 @@ public class MyFrame extends JFrame implements ActionListener{
 
                 }
             });
-            label.addMouseListener(new MouseListener() {
+            cel.addMouseListener(new MouseListener() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
 
                 }
 
+                /** wyznaczania koordynatow po kliknieciu celu
+                 * @param e
+                 */
                 @Override
+
                 public void mousePressed(MouseEvent e) {
 
                     if (pkt < lcelow-1){
                         x = xlos.nextInt(1152);
                         y = ylos.nextInt(492) + 100;
-                        label.setLocation(x, y);
+                        cel.setLocation(x, y);
 
                         pkt++;
                         klik++;
@@ -198,7 +246,7 @@ public class MyFrame extends JFrame implements ActionListener{
                         punkty.setText("Skutecznosc: "+skutecznosc + " %" + "Średnie opóźnienie: " + czas/lcelow + " ms");
                         System.out.println("winek");
                         started=false;
-                        label.setVisible(false);
+                        cel.setVisible(false);
                         poziomt.setEnabled(true);
                     }
 
@@ -223,12 +271,16 @@ public class MyFrame extends JFrame implements ActionListener{
                 }
             });
             start.addMouseListener(new MouseListener() {
+                /** dzialanie przycisku start
+                 * @param e
+                 */
                 @Override
+
                 public void mouseClicked(MouseEvent e) {
                     czas=0;
                     timer.start();
-                    label.setVisible(true);
-                    label.setLocation(x,y);
+                    cel.setVisible(true);
+                    cel.setLocation(x,y);
                     pkt=0;
                     klik=0;
                     poziomt.setEnabled(false);
@@ -265,11 +317,15 @@ public class MyFrame extends JFrame implements ActionListener{
                 }
             });
             stop.addMouseListener(new MouseListener() {
+                /** dzialanie przycisku przerywajacego gre
+                 * @param e
+                 */
                 @Override
+
                 public void mouseClicked(MouseEvent e) {
                     pkt =0;
                     klik=0;
-                    label.setVisible(false);
+                    cel.setVisible(false);
                     started = false;
                     timer.stop();
                     poziomt.setEnabled(true);
@@ -306,7 +362,11 @@ public class MyFrame extends JFrame implements ActionListener{
                 }
             });
             exit.addMouseListener(new MouseListener() {
+                /** dzialanie przycisku zamykajacego gre
+                 * @param e
+                 */
                 @Override
+
                 public void mouseClicked(MouseEvent e) {
                     System.exit(0);
                 }
@@ -334,7 +394,9 @@ public class MyFrame extends JFrame implements ActionListener{
 
         }
 
-
+    /** zliczanie czasu co tick timera i ustawianie poziomu trudnosci
+     * @param e
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         czas=czas+60;
@@ -344,24 +406,24 @@ public class MyFrame extends JFrame implements ActionListener{
                 lcelow=20;
                 celwys=64;
                 celszer=64;
-                label.setSize(celwys,celszer);
-                label.setIcon(new ImageIcon("cel.png"));
+                cel.setSize(celwys,celszer);
+                cel.setIcon(new ImageIcon("cel.png"));
 
             }
-            else if(poziomt.getSelectedItem()=="Łatwy"){
+            else if(poziomt.getSelectedItem()=="Latwy"){
                 lcelow=10;
                 celwys=96;
                 celszer=96;
-                label.setSize(celwys,celszer);
-                label.setIcon(new ImageIcon("cellatwy.png"));
+                cel.setSize(celwys,celszer);
+                cel.setIcon(new ImageIcon("cellatwy.png"));
 
             }
             else if(poziomt.getSelectedItem()=="Trudny"){
                 lcelow=25;
                 celwys=32;
                 celszer=32;
-                label.setSize(celwys,celszer);
-                label.setIcon(new ImageIcon("celtrudny.png"));
+                cel.setSize(celwys,celszer);
+                cel.setIcon(new ImageIcon("celtrudny.png"));
 
             }
         }
